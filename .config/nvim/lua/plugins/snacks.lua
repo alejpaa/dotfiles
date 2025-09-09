5c8b1af647c541ff488d8233e3d7f18145add4ac
alejpaa <alejandro.padillaa@unmsm.edu.pe>
2025-09-09 00:50:36 -0500

    add plugins

---
+++ .config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,13 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    terminal = {
+      win = {
+        position = "float", -- ahora el terminal se abre flotante
+        border = "rounded",
+        width = 0.8,
+        height = 0.8,
+      },
+    },
+  },
+}
